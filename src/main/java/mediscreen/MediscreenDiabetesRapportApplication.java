b1d379837f6a4f63107a41311dd10ddecc1b1bb7
Florent TRIMOREAU <flo2533@hotmail.fr>
2022-06-30 14:59:48 +0200

    Project Created

---
+++ src/main/java/mediscreen/MediscreenDiabetesRapportApplication.java
@@ -0,0 +1,13 @@
+package mediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenDiabetesRapportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenDiabetesRapportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mediscreen/MediscreenDiabetesRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenDiabetesRapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
